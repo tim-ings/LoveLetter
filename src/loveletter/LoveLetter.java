899c886a09bef02e29cf110707b0c9ce27bbc10e
Tim <tim@tim-ings.com>
2019-10-11 21:43:26 +0800

    Adds faster testing in java

--- src/loveletter/LoveLetter.java
+++ src/loveletter/LoveLetter.java
@@ -1,97 +1,125 @@
 package loveletter;
+
 import java.util.Random;
 import java.io.PrintStream;
 import agents.RandomAgent;
 
 /**
- * A class for running a single game of LoveLetter.
- * An array of 4 agents is provided, a deal is initialised and players takes turns until the game ends and the score is reported.
+ * A class for running a single game of LoveLetter. An array of 4 agents is
+ * provided, a deal is initialised and players takes turns until the game ends
+ * and the score is reported.
+ * 
  * @author Tim French
- * */
-public class LoveLetter{
+ */
+public class LoveLetter {
 
-  private Agent rando;
-  private Random random;
-  private PrintStream ps;
+	private Agent rando;
+	private Random random;
+	private PrintStream ps;
 
-  /**
-   * Constructs a LoveLetter game.
-   * @param seed a seed for the random number generator.
-   * @param ps a PrintStream object to record the events of the game
-   * **/
-  public LoveLetter(long seed, PrintStream ps){
-    this.random = new Random(seed);
-    this.ps = ps;
-    rando = new RandomAgent();
-  }
+	/**
+	 * Constructs a LoveLetter game.
+	 * 
+	 * @param seed a seed for the random number generator.
+	 * @param ps   a PrintStream object to record the events of the game
+	 **/
+	public LoveLetter(long seed, PrintStream ps) {
+		this.random = new Random(seed);
+		this.ps = ps;
+		rando = new RandomAgent();
+	}
 
-  /**
-   * Constructs a LoveLetter game.
-   * Defauklt construct with system random seed, and System.out as the PrintStream
-   * **/
-  public LoveLetter(){
-    this(0,System.out);
-    this.ps = System.out;
-  }
+	/**
+	 * Constructs a LoveLetter game. Defauklt construct with system random seed, and
+	 * System.out as the PrintStream
+	 **/
+	public LoveLetter() {
+		this(0, System.out);
+		this.ps = System.out;
+	}
 
+	/**
+	 * Plays a game of LoveLetter
+	 * 
+	 * @param agents the players in the game
+	 * @return scores of each agent as an array of integers
+	 **/
+	public int[] playGame(Agent[] agents) {
+		boolean gameOver = false;
+		int winner = 0;
+		int numPlayers = agents.length;
+		State gameState = new State(random, agents);// the game state
+		State[] playerStates = new State[numPlayers];
+		try {
+			while (!gameState.gameOver()) {
+				for (int i = 0; i < numPlayers; i++) {
+					playerStates[i] = gameState.playerState(i);
+					agents[i].newRound(playerStates[i]);
+				}
+				while (!gameState.roundOver()) {
+					Card topCard = gameState.drawCard();
+					Action act = agents[gameState.nextPlayer()].playCard(topCard);
+					try {
+						gameState.update(act, topCard);
+					} catch (IllegalActionException e) {
+						rando.newRound(gameState.playerState(gameState.nextPlayer()));
+						act = rando.playCard(topCard);
+						gameState.update(act, topCard);
+					}
+					for (int p = 0; p < numPlayers; p++)
+						agents[p].see(act, playerStates[p]);
+				}
+				gameState.newRound();
+			}
+			//ps.println("Player " + gameState.gameWinner() + " wins the Princess's heart!");
+			int[] scoreboard = new int[numPlayers];
+			for (int p = 0; p < numPlayers; p++)
+				scoreboard[p] = gameState.score(p);
+			return scoreboard;
+		} catch (IllegalActionException e) {
+			// ps.println("Something has gone wrong.");
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-  /**
-   * Plays a game of LoveLetter
-   * @param agents the players in the game
-   * @return scores of each agent as an array of integers
-   * **/
-  public int[] playGame(Agent[] agents){
-    boolean gameOver = false;
-    int winner=0;
-    int numPlayers = agents.length;
-    State gameState = new State(random, agents);//the game state
-    State[] playerStates = new State[numPlayers];
-    try{
-      while(!gameState.gameOver()){
-        for(int i = 0; i<numPlayers; i++){
-          playerStates[i] = gameState.playerState(i);
-          agents[i].newRound(playerStates[i]);
-        }
-        while(!gameState.roundOver()){
-          Card topCard = gameState.drawCard(); 
-          Action act = agents[gameState.nextPlayer()].playCard(topCard);
-          try{
-            gameState.update(act,topCard);
-          }
-          catch(IllegalActionException e){
-            rando.newRound(gameState.playerState(gameState.nextPlayer()));
-            act = rando.playCard(topCard);
-            gameState.update(act,topCard);
-          }
-          for(int p = 0; p<numPlayers; p++)
-            agents[p].see(act,playerStates[p]);
-        }
-        gameState.newRound();
-      }
-      ps.println("Player "+gameState.gameWinner()+" wins the Princess's heart!");
-      int[] scoreboard = new int[numPlayers];
-      for(int p = 0; p<numPlayers; p++)scoreboard[p] = gameState.score(p);
-      return scoreboard;
-    }catch(IllegalActionException e){
-      //ps.println("Something has gone wrong.");
-      e.printStackTrace();
-      return null;
-    } 
-  }
-
-  /**
-   * This main method is provided to run a simple test game with provided agents.
-   * The agent implementations should be in the default package.
-   * */
-  public static void main(String[] args){
-    Agent[] agents = {new agents.TimsAgent(),new agents.RandomAgent(), new agents.RandomAgent(), new agents.RandomAgent()};
-    LoveLetter env = new LoveLetter();
-    StringBuffer log = new StringBuffer("A simple game for four random agents:\n");
-    int[] results = env.playGame(agents);
-    env.ps.print("The final scores are:\n");
-    for(int i= 0; i<agents.length; i++)
-      env.ps.print("\t Agent "+i+", \""+agents[i]+"\":\t "+results[i]+"\n");
-  }
+	/**
+	 * This main method is provided to run a simple test game with provided agents.
+	 * The agent implementations should be in the default package.
+	 */
+	public static void main(String[] args) {
+		Random rand = new Random();
+		// get the number of tests to run from the args, deafult to 1
+		int testCount = 1;
+		if (args.length > 0) {
+			testCount = Integer.parseInt(args[0]);
+		}
+		int timWins = 0; // keep track of how many times my AI wins
+		for (int j = 0; j < testCount; j++) {
+			int timIndex = rand.nextInt(4); // randomise the position my AI is in every game
+			Agent[] agents = new Agent[4];
+			for (int i = 0; i < 4; i++) {
+				if (i == timIndex) {
+					agents[i] = new agents.TimsAgent();
+				} else {
+					agents[i] = new agents.RandomAgent();
+				}
+			}
+			LoveLetter env = new LoveLetter();
+			int[] results = env.playGame(agents);
+			int maxScoreIndex = 0;
+			int maxScore = 0;
+			for (int i = 0; i < agents.length; i++) {
+				if (results[i] > maxScore) {
+					maxScoreIndex = i;
+					maxScore = results[i];
+				}
+			}
+			if (maxScoreIndex == timIndex) {
+				timWins++;
+			}
+		}
+		float winRate = (float)timWins / (float)testCount * 100;
+		System.out.printf("Tim won %.1f%% of the time. Random chance is 25.0%%", winRate);
+	}
 }
-
-
--- test.py
+++ test.py
@@ -1,24 +1,23 @@
 from pprint import pprint
 import subprocess
 import sys
+import random
 
 
 def main():
     test_count = 10 * 100
     if len(sys.argv) >= 2:
         test_count = int(sys.argv[1])
-    results = [0] * 4
+    myWins = 0
     for x in range(test_count):
-        res = subprocess.check_output(['C:/Program Files/Java/jdk-12.0.2/bin/java.exe', '-jar', 'loveletter.jar'])
+        myIndex = random.randint(0, 3)
+        res = subprocess.check_output(['C:/Program Files/Java/jdk-12.0.2/bin/java.exe', '-jar', 'loveletter.jar', str(myIndex)])
         res_parts = str(res).split('\\n')
-        #pprint(res_parts)
         winner = res_parts[-7][9]
         winner = int(winner)
-        results[winner] += 1
-    i = 0
-    for res in results:
-        print("Result: Player {} won {} times.".format(i, res))
-        i += 1
+        if winner == myIndex:
+            myWins += 1
+    print("Result: We won {} times ({}% win rate, random rate is 25%).".format(myWins, myWins / test_count * 100))
 
 
 if __name__ == "__main__":
